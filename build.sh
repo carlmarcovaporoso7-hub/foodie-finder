a62398359e64a00330cdc92c87db40a521d3912e
Charles Vaporoso <carlmarcovaporoso7@gmail.com>
2025-11-21 18:06:44 +0800

    Fix core_profile table migration issue

--- build.sh
+++ build.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
 
-pip install --upgrade pip
 pip install -r requirements.txt
-
 python manage.py collectstatic --no-input
-python manage.py migrate auth
-python manage.py migrate contenttypes
-python manage.py migrate admin
-python manage.py migrate sessions
-
-# Create superuser
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'adminpass123')" | python manage.py shell
+python manage.py makemigrations
+python manage.py migrate
