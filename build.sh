2e699bc8f16ac088fab0948361cc198ad9a7307a
Charles Vaporoso <carlmarcovaporoso7@gmail.com>
2025-11-21 17:57:43 +0800

    Fix migration issue in build script

--- build.sh
+++ build.sh
@@ -5,7 +5,10 @@ pip install --upgrade pip
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py migrate
+python manage.py migrate auth
+python manage.py migrate contenttypes
+python manage.py migrate admin
+python manage.py migrate sessions
 
-# Create superuser if it doesn't exist
+# Create superuser
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'adminpass123')" | python manage.py shell
