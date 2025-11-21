bfcc7d9cbf9a3b22e73e936da7a4af53c4d0a038
Charles Vaporoso <carlmarcovaporoso7@gmail.com>
2025-11-21 18:29:16 +0800

    Remove problematic seed data migration during build

--- build.sh
+++ build.sh
@@ -3,4 +3,7 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py migrate --fake-initial
+python manage.py migrate
+
+# Create superuser
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'adminpass123')" | python manage.py shell
